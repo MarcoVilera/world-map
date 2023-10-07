46ce5fd2083de1823d2b88ab6650c9794583b899
MarcoVilera <marcovilarev123@gmail.com>
2023-10-07 00:31:14 -0400

    refactor: structure changed

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 console.log("Prueba")
 //TODO Implementar Rotación Img
 //TODO Implementar Reproducción de sonido
+const pinIMG = document.getElementById("mappin")
+const pinDiv = document.querySelector(".dot")
+const worldDiv = document.querySelector(".world")
+
+const pinWidth = pinDiv.offsetWidth
+const pinHeight = pinDiv.offsetHeight
+
+let randomX = Math.floor(Math.random() * pinWidth)
+let randomY = Math.floor(Math.random() * pinHeight)
+pinIMG.style.visibility = "visible"
+randomX = 105
+randomY = 215
+//worldDiv.style.backgroundPosition = "150px"; Cambia la posición del fondo del div
+pinIMG.style.left = randomX + "px"
+pinIMG.style.top = randomY + "px"
